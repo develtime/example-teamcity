dc7b592d8abf4cc7df1181dd8277121d7f290ec6
Vadim Smirnov <smirnov.v.vadim@icloud.com>
2021-12-09 17:54:07 +0300

    change versions

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,8 +1,8 @@
-import jetbrains.buildServer.configs.kotlin.v2021.2.*
-import jetbrains.buildServer.configs.kotlin.v2021_2.buildSteps.maven
-import jetbrains.buildServer.configs.kotlin.v2021_2.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.v2021_2.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.v2021_2.vcs.GitVcsRoot
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -26,7 +26,7 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 'Debug' option is available in the context menu for the task.
 */
 
-version = "2021.2"
+version = "2019.2"
 
 project {
 
