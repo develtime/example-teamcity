19ce143435092cdaac7f377440f32547c7a558a4
Vadim Smirnov <smirnov.v.vadim@icloud.com>
2021-12-09 18:01:34 +0300

    add some test

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -22,5 +22,8 @@ public class WelcomerTest {
 		assertThat(welcomer.sayWelcome(), containsString("hunter"));
 		assertThat(welcomer.sayFarewell(), containsString("hunter"));
 	}
-
+	@Test
+	public void welkomerSaysSomething() {
+		assertThat(welcomer.saySomething(), containsString("hunter"));
+	}
 }
